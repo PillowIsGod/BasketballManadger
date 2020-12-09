0484a294a938a2dca7e830ee21cfbbacb6978524
Eugene Pecherskiy <Chudik.2014@gmail.com>
2020-12-09 22:03:37 +0200

    added mysql db

--- BasketballManadger/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ BasketballManadger/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -349,7 +349,7 @@ namespace BasketballManadger {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/BasketballManadger;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/BasketballManadger;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
